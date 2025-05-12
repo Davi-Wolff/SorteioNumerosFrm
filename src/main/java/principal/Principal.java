04939913064bc4d15a71e4654ae49c7ac09d91fa
Davi Wolff <>
2025-05-12 15:47:15 -0300

    Início do projeto.

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,14 @@
+package principal;
+
+import view.FrmSorteio;
+
+/**
+ *
+ * @author Davi Wolff
+ */
+public class Principal {
+    public static void main(String[] args) {
+        FrmSorteio janela = new FrmSorteio();
+        janela.setVisible(true);
+    }
+}
